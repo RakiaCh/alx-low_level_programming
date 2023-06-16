c13b47b1eb85d814cafa898819729bda5098a23a
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-16 15:28:24 -0700

    My first mandatory commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints hexadecimal base 0123456789abcdef, using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n ;
+	int alpha;
+
+	for (n = '0' ; n <= '9'; n++) /*print 0-9*/
+	{
+		putchar(n);
+	}
+
+	for (alpha = 'a' ; alpha <= 'f' ; alpha++) /*print a-f to finish hexbase*/
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print 0-9 separated with commas, using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n ;
+
+	for (n='0' ; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
