77eef2c576237ba00e21579a9545128d6cd07a1c
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-20 15:36:33 -0700

    second commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,21 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	while (n < 98)
 	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf(", ");
-			printf("%d", i);
-		}
+		printf("%d, ", n);
+		n++;
 	}
-	else
+
+	while (n > 98)
 	{
-		for (i = n ; i >= 98 ; i--)
-		{
-			printf(", ");
-			printf("%d", i);
-		}
+		printf("%d, ", n);
+		n--;
 	}
+
+	if (n == 98)
+		printf("%d", n);
+
+	printf("\n");
 
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,10 @@
 
 int print_last_digit(int n)
 {
-	int x;
+	if (n < 0)
+		n *= -1;
 
-	x = abs(n);
+       	_putchar('0' + (n % 10));
 
-	_putchar('0' + (x % 10));
-
-	return (x % 10);
+	return (n % 10);
 }
