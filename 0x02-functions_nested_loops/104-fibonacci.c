6462f459f5cb591f770247f8a017e84417fe43b0
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-20 16:45:40 -0700

    my first advanced commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1;
+	long a = 1;
+	long b = 2;
+
+	while (i < (98 / 2))
+	{
+		printf("%li, %li, ", a, b);
+		a += b;
+		b += a;
+		i++;
+	}
+
+	printf("%li, %li", a, b);
+
+	printf("\n");
+
+	return (0);
+}
