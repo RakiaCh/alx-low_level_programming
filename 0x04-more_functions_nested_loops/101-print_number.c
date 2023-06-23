0b6d75409db3bad652d9d3ad8db00ee8f5094316
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-22 17:27:20 -0700

    second commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,31 +5,22 @@
  * @n: number to be printed
  */
 
-void print_number(int n)
-{
-	if (n == 0)
-	{
-		_putchar('0');
-	}
+void print_number(int n) {
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+    int divisor = 1;
+    while (divisor <= n / 10) {
+        divisor *= 10;
+    }
 
-	int reversed = 0;
-
-	while (n > 0)
-	{
-		reversed = reversed * 10 + (n % 10);
-		n /= 10;
-	}
-
-	while (reversed > 0)
-	{
-		_putchar('0' + (reversed % 10));
-		reversed /= 10;
-	}
+    while (divisor > 0) {
+        int digit = n / divisor;
+        _putchar('0' + digit);
+        n %= divisor;
+        divisor /= 10;
+    }
 }
 
