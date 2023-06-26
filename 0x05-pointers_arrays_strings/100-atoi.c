3ba5afd1cf58dd5974ec98b6dc5ab1f4d71f3749
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-26 16:15:43 -0700

    first advanced commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,11 +20,18 @@ int _atoi(char *s)
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
+	while (s[i] != '\0')
 	{
-		result = result * 10 + (s[i] - '0');
-		i++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			i++;
+		}
 	}
 
-	return (sign*result);
+	return (sign * result);
 }
