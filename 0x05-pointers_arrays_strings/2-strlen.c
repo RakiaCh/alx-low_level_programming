8465376effec8c51e4e274d8b4a630ce9fec70c0
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-26 13:06:35 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -  a function that returns the length of a string
+ * @s: the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(*s);
+
+	return (length);
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - a function that prints a string, in reverse, followed by a new line
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = strlen(*s);
+
+	for (i = len ; i > 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+
