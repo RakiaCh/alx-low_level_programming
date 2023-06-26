f09c3d450c80cff38f534095ca03686a917badd3
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-26 15:10:59 -0700

    my second commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - a function that copies the string pointed to by src,
+ * including (\0), to the buffer pointed to by dest
+ * @src: source
+ * @dest: destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
