82f364319d1603a282dc728fcb6c6f6286230c8e
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-26 13:13:34 -0700

    my first commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,6 +10,6 @@ int _strlen(char *s)
 {
 	int length;
 
-	length = strlen(*s);
+	length = strlen(s);
 
 	return (length);
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int len;
 	int i;
 
-	len = strlen(*s);
+	len = strlen(s);
 
 	for (i = len ; i > 0 ; i--)
 	{
