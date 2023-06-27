c7df403635daa466a78fb7c48597eef9c27f02af
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-26 17:24:35 -0700

    first advanced commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 int main(void)
 {
-	int i;
-	int randomIndex;
-	const char validChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	srand(time(NULL));
-
-	for (i = 0; i < 10; i++)
-	{
-		randomIndex = rand() % (sizeof(validChars) - 1);
-		putchar(validChars[randomIndex]);
-	}
-
-	putchar('\n');
+	int password;
+	srand(time(0));
+	password = rand();
 	
-	return 0;
+	printf("%i\n", password);
+	
+	return (0);
 }
-
