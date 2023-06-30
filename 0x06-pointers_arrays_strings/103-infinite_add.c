77afa0c8cf5f85c6af16aa9d978b160273c50905
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-06-30 14:39:25 -0700

    second advanced commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,52 +13,36 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1;
-	int len2;
-	int max_len;
-	int carry;
-	int i;
-	int j;
-	int k;
+	int len1, len2, max_len, carry;
+	int i, j, k;
 	int digit, digit1, digit2;
-	int sum;
-	int left, right;
+	int sum, left, right;
 	char temp;
-
 	len1 = strlen(n1);
 	len2 = strlen(n2);
 	max_len = (len1 > len2) ? len1 : len2;
-
 	if (max_len >= size_r - 1)
 	{
 		return 0;
 	}
-
 	i = len1 - 1;
 	j = len2 - 1;
 	k = 0;
-
 	while (i >= 0 || j >= 0 || carry > 0)
 	{
 		digit1 = (i >= 0) ? n1[i] - '0' : 0;
 		digit2 = (j >= 0) ? n2[j] - '0' : 0;
-
 		sum = digit1 + digit2 + carry;
 		carry = sum / 10;
 		digit = sum % 10;
-
 		r[k] = digit + '0';
-
 		i--;
 		j--;
 		k++;
 	}
-
 	r[k] = '\0';
-
 	left = 0;
 	right = k - 1;
-
 	while (left < right)
 	{
 		temp = r[left];
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void print_buffer(char *b, int size) {
+    int i, j, count;
+    unsigned char c;
+    count = 0;
+
+    if (size <= 0) {
+        printf("\n");
+        return;
+    }
+
+    for (i = 0; i < size; i += 10) {
+        printf("%08x ", i);
+
+        for (j = i ; j < i + 10; j++) {
+            if (j < size) {
+                printf("%02x", b[j] & 0xFF);
+                count++;
+                if ((count % 2) == 0)
+                   printf(" ");
+                
+            } else {
+                printf(" ");
+            }
+        }
+
+        printf(" ");
+
+        for (j = i ; j < i + 10; j++) {
+            if (j < size ) {
+                c = b[j];
+                printf("%c", isprint(c) ? c : '.');
+            } else {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+}
