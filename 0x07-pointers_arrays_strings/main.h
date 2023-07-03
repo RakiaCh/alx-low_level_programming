e371ee83681824104ffa37a22a6bd34184d75183
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-07-03 15:09:34 -0700

    my first advanced commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -39,4 +39,8 @@ void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
 
+/* function that sets the value of a pointer to a char. */
+
+void set_string(char **s, char *to);
+
 #endif /* MAIN_H */
