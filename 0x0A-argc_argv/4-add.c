c1fd867459ccccd715f76a8052fe9a1e3a8dffa1
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-07-07 13:44:09 -0700

    my first commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		if (isdigit(argv[i]))
+		{
+			printf("Error\n");
+                        return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+/* _putchar : writes the character c to stdout */
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
