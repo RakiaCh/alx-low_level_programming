686d7e665e1d5f3f0855ee43f12f869c46a4218a
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-07-19 14:48:38 -0700

    my first advanced commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,28 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	int num_bytes, i;
-       	char *ptr;
+	register int i, num;
+	char *ptr = (char *)main;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	num_bytes = atoi(argv[1]);
-	if (num_bytes <= 0)
-	{
-		printf("Error\n");
-		return (2);
-	}
-
-	ptr = (char *)main;
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x", ptr[i]);
-	}
-	printf("\n");
-
+		printf("Error\n"), exit(1);
+	num = atoi(argv[1]);
+	if (num < 0)
+		printf("Error\n"), exit(2);
+	for (i = 0; i < num - 1; i++)
+		printf("%02hhx ", ptr[i]);
+	printf("%02hhx\n", ptr[i]);
 	return (0);
 }
