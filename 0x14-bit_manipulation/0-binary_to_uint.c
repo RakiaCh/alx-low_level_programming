3e0dc21cdfcc7bd1d12d9e1e99cc60c84bec4897
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-08-04 14:13:33 -0700

    my first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	int len = 0, i;
-	unsigned int sum = 0;
+	unsigned int num = 0, mult = 1;
+	int len;
 
 	if (b == NULL)
-		return (sum);
+		return (0);
 
 	while (b[len] != '\0')
 		len++;
-	len -= 1;
 
-	i = 0;
-	while (b[i])
+	for (len -= 1; len >= 0; len--)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
-	return (sum);
+	return (num);
 }
