dfbb4912b3a3fbd50af96f1c024b9f14128c41c5
Rakia Chebbah <chebbahrakia14@gmail.com>
2023-08-07 14:54:46 -0700

    my first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,40 +9,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_wrote;
+	ssize_t op, n_read, n_wrote;
 	char *buffer;
 
-	if (!filename)
-		return (0)
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-
-	if (!buffer)
+	if (buffer == NULL)
 		return (0);
 
-	n_read = read(fd, buffer, letters);
-
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
+	op = open(filename, O_RDONLY);
+	n_read = read(op, buffer, letters);
 	n_wrote = write(STDOUT_FILENO, buffer, n_read);
 
-	if (n_wrote == -1)
+	if (op == -1 || n_read == -1 || n_wrote == -1 || n_wrote != n_read)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
 
-	close(fd);
-	return (n_read);
+	free(buffer);
+	close(op);
 
+	return (n_wrote);
 }
